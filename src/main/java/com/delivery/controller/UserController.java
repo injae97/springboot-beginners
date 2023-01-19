13bd8b068f0a79be4f33c4d086b9ca7d469629b7
InJeKim97 <injaekim97@naver.com>
2023-01-19 22:04:51 +0900

    delivery website in development v0.1

--- src/main/java/com/delivery/controller/UserController.java
+++ src/main/java/com/delivery/controller/UserController.java
@@ -1,6 +1,15 @@
 package com.delivery.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -25,11 +34,30 @@ public class UserController {
 	 * 파라미터 값들은 DTO에 생성
 	 */
 	@PostMapping("/join")
-	public String joinProc(Join join) {
+	public String joinProc(@Valid Join join, BindingResult bindingResult, Model model) {
 		
-		/* 회원가입 Form 에서 입력한 데이터 출력 */
+		/* 회원가입 Form 에서 입력한 데이터 출력(get 이용) 
 		System.out.println("Username=" + join.getUsername() + ", " + "Password=" + join.getPassword() + ", " +
-	                        "Email=" + join.getEmail() + ", " + "Nickname=" + join.getNickname() + ", " +  "Phone=" + join.getPhone());
+	                        "Email=" + join.getEmail() + ", " + "Nickname=" + join.getNickname() + ", " +  "Phone=" + join.getPhone()); */
+		
+		if (bindingResult.hasErrors()) {
+			System.out.println("에러 발생");
+		
+			List<FieldError> list = bindingResult.getFieldErrors(); // 에러정보를 확인
+			Map<String, String> errorMsg = new HashMap<>();
+			
+			for(int i=0; i<list.size(); i++) {
+				String field = list.get(i).getField();
+				String message = list.get(i).getDefaultMessage();
+				
+				System.out.println("필드 = " + field);
+				System.out.println("메세지 = " + message);
+				
+				errorMsg.put(field, message); // 에러난 필드와 메세지를 Map에 담음
+			}
+			model.addAttribute("errorMsg" , errorMsg); // model 속성에 errorMsg 변수명으로 errorMsg 값을 넣어줌
+			return "user/join";
+		}			
 		return "redirect:/login";
 	}
 
--- src/main/java/com/delivery/dto/Join.java
+++ src/main/java/com/delivery/dto/Join.java
@@ -1,16 +1,29 @@
 package com.delivery.dto;
 
+import javax.validation.constraints.Pattern;
+
 import lombok.ToString;
 
 @ToString
 /* join.jsp 에서 회원가입 정보 입력칸 (form 태그 input 태그의 값들) name으로 준 값들 선언 */
+/* @Pattern 으로 회원가입시 유효성검사 체크 가능 */
 public class Join {
+	
+	@Pattern(regexp = "[A-Za-z0-9]{4,15}$", message = "아이디는 영어, 숫자 4 ~15자리로 입력 가능합니다")
 	private String username;
+
     private String password;
+    
+    @Pattern(regexp = "^([0-9a-zA-Z_\\.-]+)@([0-9a-zA-Z_-]+)(\\.[0-9a-zA-Z_-]+){1,2}$" , message = "올바른 이메일 형식이 아닙니다")
     private String email;
+    
+    @Pattern(regexp = "^[가-힣|a-z|A-Z|0-9|]+$", message = "닉네임은 한글, 영어, 숫자만 4 ~10자리로 입력 가능합니다")
     private String nickname;
+    
+    @Pattern(regexp = "^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$", message = "휴대폰번호를 확인해 주세요")
     private String phone;
     
+    /* Getter & Setter */
 	public String getUsername() {
 		return username;
 	}
