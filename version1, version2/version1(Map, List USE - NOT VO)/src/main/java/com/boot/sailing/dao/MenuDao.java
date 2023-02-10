5edc340f7d1a58e566e6b7c14f092da37c230966
injaekim97 <injaekim97@naver.com>
2023-02-10 23:21:02 +0900

    Version1 (Map, List  사용 - NOT VO)

---
+++ version1, version2/version1(Map, List USE - NOT VO)/src/main/java/com/boot/sailing/dao/MenuDao.java
@@ -0,0 +1,42 @@
+package com.boot.sailing.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Mapper
+public interface MenuDao {
+
+	/* [SELECT] - 메뉴 전체 조회  */
+	List<Map<String, Object>> doList();
+	
+	/* [INSERT] - 메뉴 등록 */
+	int doInsert(@Param("strCoffee") String coffee, @Param("strKind")  String kind, @Param("strPrice")  String price);
+
+	/* [DELETE] - 메뉴 삭제 */
+	int doDelete(String strNo);
+
+	/* [SELECT] - 수정 클릭 시 해당 데이터 값 호출(doListOne), UPDATE를 위한 용도 */
+	Map<String, Object> doListOne(String strNo);
+
+	/* [UPDATE] - 메뉴 수정 */
+	int doUpdate(@Param("strNo") String no, @Param("strCoffee") String coffee, @Param("strKind") String kind, @Param("strPrice") String price);
+
+	/* [SELECT] - 검색 기능(Search) */
+	List<Map<String, Object>> doSearch(@Param("strStartDate") String start_date, @Param("strEndDate") String end_date, @Param("strCoffee") String coffee, @Param("strKind") String kind);
+
+	/* [UPDATE] - 다중 가격 데이터 수정 */
+	int doUpdatePrice(@Param("strNo") String no, @Param("strPrice") String price);
+	
+	/* [INSERT] - 수정한 가격 데이터 등록 */
+	int doInsertLog(@Param("strNo") String no, @Param("strPrice") String price);
+
+	/* [UPDATE] - 다중 가격 데이터 수정(원 쿼리 - doUpdatePriceOne) */
+	int doUpdatePriceOne(@Param("chkList") List<String> chkCoffeeNo, @Param("strPrice") String price);
+
+	/* [INSERT] - 수정한 가격 데이터 등록(원 쿼리 - doInsertLogOne) */
+	int doInsertLogOne(@Param("chkList") List<String> chkCoffeeNo, @Param("strPrice") String price);
+}
