232b00b33f5cba54905ea31857b37863ec4367b6
injaekim97 <injaekim97@naver.com>
2023-02-10 23:21:40 +0900

    Version2 (VO 사용 - NOT Map, List )

---
+++ version1, version2/version2(VO USE - NOT Map, List)/src/main/java/com/boot/sailing/controller/MenuCon.java
@@ -0,0 +1,162 @@
+package com.boot.sailing.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.boot.sailing.service.MenuSvc;
+import com.boot.sailing.vo.Coffee_menu;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@Log4j2
+public class MenuCon {
+	
+	// @Autowired를 통해 주입 받음
+	@Autowired
+	MenuSvc menuSvc;
+	
+	/*
+	 * [SELECT] - 메뉴 전체 조회 	
+	 * 해당 검색 결과만 받기 위하여 List<Coffee_menu> Vo 사용
+	 */
+	@GetMapping("/menu")
+	public String doMenu(Model model) {
+
+		List<Coffee_menu> list = menuSvc.doList(); // Vo 사용
+		
+		model.addAttribute("list", list);
+		model.addAttribute("hello", "========== MenuCon VO 사용 ==========");
+		
+		System.out.println("@@@@@@@@@list@@@@@@@@@@" + list);
+		return "/menu/menu"; 
+	}  
+	
+    /*
+     * [INSERT] - 메뉴 등록 1 
+     * 화면 이동이기 때문에 @GetMapping 사용
+     * localhost:8080/menu_ins로 들어오면 /menu/menu_ins.html 화면을 보여줌
+     */
+	@GetMapping("/menu_ins")
+	public String doInsert() {
+		return "/menu/menu_ins";
+	}
+	
+	/* [INSERT] - 메뉴 등록 2 */
+	@PostMapping("/menu_ins")
+	public String doInsertPost(
+			@RequestParam("coffee") String strCoffee, 
+			@RequestParam("kind") String strKind, 
+			@RequestParam("price") String strPrice )	
+	{
+        log.info("==========================================================");
+        log.info(strCoffee);
+		int intI = menuSvc.doInsert(strCoffee, strKind, strPrice);
+		return "redirect:/menu"; // return은 @RequestMapping이 적용되지 않는다.
+	}
+	
+	/* [DELETE] - 메뉴 삭제 */
+	@GetMapping("/menu_del")
+	public String doDelete(@RequestParam("no") String strNo) {
+        log.info("==========================================================");
+        log.info("strNo:" + strNo);
+        
+		int intI = menuSvc.doDelete(strNo);
+
+		return "redirect:/menu"; 
+	}
+	
+	
+	/* [SELECT] - 수정 클릭 시 해당 데이터 값 호출(doListOne), UPDATE를 위한 용도 */
+	@GetMapping("/menu_up")
+	public String doUpdate(Model model, @RequestParam("no") String strNo) {
+
+		Map<String, Object> map = menuSvc.doListOne(strNo);
+		
+		model.addAttribute("map", map);
+
+		return "/menu/menu_up"; 
+	}
+	
+	/* [UPDATE] - 메뉴 수정 */
+	@PostMapping("/menu_up")
+	public String doUpdatePost(
+			@RequestParam("no") String strNo, 
+			@RequestParam("coffee") String strCoffee, 
+			@RequestParam("kind") String strKind, 
+			@RequestParam("price") String strPrice )	
+	{
+		int intI = menuSvc.doUpdate(strNo, strCoffee, strKind, strPrice);
+		return "redirect:/menu"; // return은 @RequestMapping이 적용되지 않는다.
+	}
+	
+	
+	/*
+	 * [SELECT] - 검색 기능(Search)
+	 * 해당 검색 결과만 받기 위하여 List<Map<String, Object>> list 사용
+	 * Model model로 menu.html에 있는 <tr th:each="prod : ${list}">을 뿌려주기 위해 list로 넘겨줌
+	 */
+	@PostMapping("/menu_search")
+	public String doSearch(
+			@RequestParam("start_date") String strStartDate, 
+			@RequestParam("end_date") String strEndDate, 
+			@RequestParam(value = "coffee", defaultValue = "ALL") String strCoffee,  /* null이 올수 있는 경우에 defaultValue = "ALL"를 넣어주면 좋다. */
+			@RequestParam("kind") String strKind,
+			Model model)	
+	{
+		log.info("==========================================================");
+		log.info("start_date:" + strStartDate);
+		List<Coffee_menu> list = menuSvc.doSearch(strStartDate, strEndDate, strCoffee, strKind);
+		model.addAttribute("list", list);
+		return "/menu/menu"; 
+	}
+	
+	
+	
+	/*
+	 * ★★★ [문제] - for loop 안에서 사용 했기 때문에 효율적이지 못함 ★★★
+	 * [UPDATE] - 다중 가격 데이터 수정
+     * [INSERT] - 수정한 가격 데이터 등록
+	 * menu.html 에서 <td><input type="checkBox" name="chkCoffeeNo" th:value="${prod.get('no')}"></td> 의 name값을 @RequestParam 적음
+	 * menu.html 에서 <input type="hidden" name="hidden_price">의 name값을 @RequestParam 적음
+
+	@PostMapping("/menu_updatePrice")
+	public String doUpdatePrice(@RequestParam("chkCoffeeNo") List<String> chkList, @RequestParam("hidden_price") String strPrice) {
+		
+		if(chkList != null) {
+			// for(변수 선언(type 변수명) : 배열)
+			for(String strNo : chkList) {
+				
+				int int1 = menuSvc.doUpdatePrice(strNo, strPrice); // [UPDATE] - 다중 가격 데이터 수정
+				int int2 = menuSvc.doInsertLog(strNo, strPrice); // [INSERT] - 수정한 가격 데이터 등록
+			}
+		}
+			
+		return "redirect:/menu";
+	}
+	*/
+	
+	
+	/*
+	 * 한번에 하나만 효율적으로 사용 
+	 * [UPDATE] - 다중 가격 데이터 수정(원 쿼리 - 1번에 처리하는 것을 뜻함)
+     * [INSERT] - 수정한 가격 데이터 등록(원 쿼리 - 1번에 처리하는 것을 뜻함)
+	 * menu.html 에서 <td><input type="checkBox" name="chkCoffeeNo" th:value="${prod.get('no')}"></td> 의 name값을 @RequestParam 적음
+	 * menu.html 에서 <input type="hidden" name="hidden_price">의 name값을 @RequestParam 적음
+	 */
+	@PostMapping("/menu_updatePrice")
+	public String doUpdatePrice(@RequestParam("chkCoffeeNo") List<String> chkList, @RequestParam("hidden_price") String strPrice) {
+		
+		int int1 = menuSvc.doUpdatePriceOne(chkList, strPrice);
+		int int2 = menuSvc.doInsertLogOne(chkList, strPrice);
+	
+		return "redirect:/menu";
+	}
+}
---
+++ version1, version2/version2(VO USE - NOT Map, List)/src/main/java/com/boot/sailing/service/MenuSvc.java
@@ -0,0 +1,130 @@
+package com.boot.sailing.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.boot.sailing.dao.MenuDao;
+import com.boot.sailing.vo.Coffee_menu;
+
+import lombok.extern.log4j.Log4j2;
+
+@Service 
+@Log4j2
+public class MenuSvc {
+	
+	@Autowired
+	MenuDao menuDao;
+	
+	public MenuSvc() {
+		log.info("================ MenuSvc , 생성자 ===================");
+	}
+	
+	/* [SELECT] - 메뉴 전체 조회(Vo 사용)  */
+	public List<Coffee_menu> doList() {
+
+		List<Coffee_menu> list = menuDao.doList();
+		
+		log.info(list);
+		return list;	
+	}
+	
+	
+	// Data Create - List, Map(Old)
+	public List<Map<String, Object>> doListOld() {
+		List<Map<String, Object>> list = new ArrayList<>();
+		Map<String, Object> map = new HashMap<>();
+
+		map.put("no", "1");
+		map.put("coffee", "아이스아메리카노");
+		map.put("kind", "커피");
+		map.put("price", "5,000");
+		map.put("reg_day", "2020.10.29");
+		map.put("mod_day", "2021.10.29");
+		list.add(map);
+
+		Map<String, Object> map2 = new HashMap<>();
+
+		map2.put("no", "2");
+		map2.put("coffee", "카푸치노");
+		map2.put("kind", "커피");
+		map2.put("price", "6,000");
+		map2.put("reg_day", "2020.10.30");
+		map2.put("mod_day", "2021.10.30");
+		list.add(map2);
+		
+		Map<String, Object> map3 = new HashMap<>();
+
+		map3.put("no", "3");
+		map3.put("coffee", "카라멜 마끼야또");
+		map3.put("kind", "커피");
+		map3.put("price", "7,000");
+		map3.put("reg_day", "2020.10.31");
+		map3.put("mod_day", "2021.10.31");
+		list.add(map3);
+		
+		log.info(list);
+
+		return list;
+	}
+
+	/* [INSERT] - 메뉴 등록 */
+	public int doInsert(String strCoffee, String strKind, String strPrice) {
+		int intI = menuDao.doInsert(strCoffee, strKind, strPrice);
+		return intI;
+	}
+
+	/* [DELETE] - 메뉴 삭제 */
+	public int doDelete(String strNo) {
+		int intI = menuDao.doDelete(strNo);
+		return intI;
+	}
+	
+	/* [SELECT] - 수정 클릭 시 해당 데이터 값 호출(doListOne), UPDATE를 위한 용도 */
+	public Map<String, Object> doListOne(String strNo) {
+		Map<String, Object> map = menuDao.doListOne(strNo);
+
+		return map;
+	}
+
+	/* [UPDATE] - 메뉴 수정 */
+	public int doUpdate(String strNo, String strCoffee, String strKind, String strPrice) {
+		int intI = menuDao.doUpdate(strNo, strCoffee, strKind, strPrice);
+		return intI;
+	}
+
+	/* [SELECT] - 검색 기능(Search) */
+	public List<Coffee_menu> doSearch(String strStartDate, String strEndDate, String strCoffee, String strKind) {
+		
+		List<Coffee_menu> list = menuDao.doSearch(strStartDate, strEndDate, strCoffee, strKind);
+		return list;
+	}
+
+	/* [UPDATE] - 다중 가격 데이터 수정 */
+	public int doUpdatePrice(String strNo, String strPrice) {
+		int int1 = menuDao.doUpdatePrice(strNo, strPrice);
+		return int1;
+	}
+
+	/* [INSERT] - 수정한 가격 데이터 등록 */
+	public int doInsertLog(String strNo, String strPrice) {
+		int int2 = menuDao.doInsertLog(strNo, strPrice);
+		return int2;
+	}
+	
+	/* [UPDATE] - 다중 가격 데이터 수정(원 쿼리 - doUpdatePriceOne) */
+	public int doUpdatePriceOne(List<String> chkList, String strPrice) {
+		int int1 = menuDao.doUpdatePriceOne(chkList, strPrice); 		
+		return int1;
+	}
+	
+	/* [INSERT] - 수정한 가격 데이터 등록(원 쿼리 - doInsertLogOne) */
+	public int doInsertLogOne(List<String> chkList, String strPrice) {
+		int int2 = menuDao.doInsertLogOne(chkList, strPrice); 	
+		return int2;
+	}
+}
